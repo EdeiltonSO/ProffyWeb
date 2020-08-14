c13587c05f7e48f900504c60cb6801b82a36d41b
Edeilton Oliveira <edeiltonso@gmail.com>
2020-08-14 17:43:40 -0300

    added api search and typescript

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,32 +2,49 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string,
+}
+
+interface TeacherItemProps {
+    teacher: Teacher,
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection(){
+        api.post('/connections', {
+            user_id: teacher.id
+        })
+    }
+
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/38273608?s=460&u=abd1bf32e2819b46e2980da735678508cb0ca8ea&v=4" alt="Edeilton Oliveira"/>
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Edeilton Oliveira</strong>
-                    <span>Algoritmos e Programação</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de programação.
-                <br/><br/>
-                The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
-                    Preço/hora <strong>R$ 80,00</strong>
+                    Preço/hora <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a onClick={createNewConnection} target="_blank" rel="noopener noreferrer" href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="WhatsApp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
